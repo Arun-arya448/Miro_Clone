ca0068ce0d7057ec1944c458fc38bdafbd9a0772
Arunarya <arya991160@gmail.com>
2025-09-03 05:58:10 +0530

    Added invite members button + search input debounce fix

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useOrganization,useOrganizationList } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps{
     id: string;
@@ -28,6 +29,7 @@ export const Item = ({
 
     return(
         <div className="aspect-square relative">
+            <Hint label={name} side="right" align="start" sideOffset={18}>
             <Image 
                 fill 
                 alt="name" 
@@ -36,6 +38,7 @@ export const Item = ({
                 className={cn("rounded-md curser-pointer opacity-75 hover:opacity-100 transition",
                     isActive && "opacity-100" 
                 )}/>
+            </Hint>    
         </div>
     );
 };
